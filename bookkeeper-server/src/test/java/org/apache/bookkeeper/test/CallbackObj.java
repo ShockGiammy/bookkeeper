7cf166aec1131ce5475bc0366f9226a05d57ca49
ShockGiammy <gian.marco.1698@gmail.com>
2021-06-22 16:16:18 +0200

    test client closed

---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/test/CallbackObj.java
@@ -0,0 +1,17 @@
+package org.apache.bookkeeper.test;
+
+import java.util.concurrent.CountDownLatch;
+
+class CallbackObj {
+    int rc;
+    long requested;
+    long freeDiskSpace, totalDiskCapacity;
+    CountDownLatch latch = new CountDownLatch(1);
+
+    CallbackObj(long requested) {
+        this.requested = requested;
+        this.rc = 0;
+        this.freeDiskSpace = 0L;
+        this.totalDiskCapacity = 0L;
+    }
+}
